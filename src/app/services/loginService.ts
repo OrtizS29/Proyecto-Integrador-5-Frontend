56c8cc84c28d41e12d6921792f327f688ec76c62
Santiago Ortiz <santiago.ortizb06@gmail.com>
2025-05-30 18:36:29 -0500

    Se hace cambios para que el login funcione.

--- src/app/services/loginService.ts
+++ src/app/services/loginService.ts
@@ -2,12 +2,15 @@
 // auth.service.ts
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../models/LoginResponse';
+
 
 @Injectable({ providedIn: 'root' })
 export class Login {
   constructor(private http: HttpClient) {}
 
-  loginConToken(token: string) {
-    return this.http.post('https://proyecto-integrador-5-backend.onrender.com/api/auth/login', { token });
+  loginConToken(token: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>('https://proyecto-integrador-5-backend.onrender.com/api/auth/login', { token });
   }
 }
